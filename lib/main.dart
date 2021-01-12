ae69ef906310a050e1fe503ce8f0ccd106357e89
lParanhos <leandro.paranhos@dextra-sw.com>
2021-01-11 23:26:58 -0300

    Create Transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(Expenses());
@@ -12,6 +13,21 @@ class Expenses extends StatelessWidget {
 }
 
 class MyHome extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
